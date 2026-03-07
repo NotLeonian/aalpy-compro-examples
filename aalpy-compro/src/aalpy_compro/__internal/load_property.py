53ac58b964a8d4b4dc7e693d9803cc3643bafbfe
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-03-08 01:36:12 +0900

    `load_property` の返す例外のメッセージの一部が f 文字列になっていなかったのを修正

--- aalpy-compro/src/aalpy_compro/__internal/load_property.py
+++ aalpy-compro/src/aalpy_compro/__internal/load_property.py
@@ -31,9 +31,9 @@ def load_property(path: str) -> LearningProperty[object]:
     spec.loader.exec_module(mod)
 
     if not hasattr(mod, "alphabet"):
-        raise ValueError("`alphabet` must be defined in {path}.")
+        raise ValueError(f"`alphabet` must be defined in {path}.")
     if not hasattr(mod, "accepts"):
-        raise ValueError("`accepts` must be defined in {path}.")
+        raise ValueError(f"`accepts` must be defined in {path}.")
 
     alphabet = getattr(mod, "alphabet")
     accepts = getattr(mod, "accepts")
