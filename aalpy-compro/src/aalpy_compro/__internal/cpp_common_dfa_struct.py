ca8bb98f9271c74c5c3722f35560110c25b4ff0e
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-03-09 10:50:10 +0900

    cpp_common_dfa_struct.py の文字列の多くをトリプルクォートに変更

--- aalpy-compro/src/aalpy_compro/__internal/cpp_common_dfa_struct.py
+++ aalpy-compro/src/aalpy_compro/__internal/cpp_common_dfa_struct.py
@@ -18,132 +18,120 @@ def common_dfa_struct(*, namespace: str = "learned_dfa") -> str:
         res.append(f"#include <{header_name}>")
     res.append("")
     res.append(f"namespace {namespace} {{")
-    res.append("class DFA {")
-    res.append("  private:")
-    res.append("    int n;")
-    res.append("    int sigma;")
-    res.append("    int initial_state;")
-    res.append("")
-    res.append("    std::vector<unsigned char> accepting;")
-    res.append("    std::vector<std::vector<int>> trans;")
-    res.append("")
-    res.append("    std::string key;")
-    res.append("")
-    res.append("    // initial_state や trans などが範囲外となることも一応許容する")
-    res.append("")
-    res.append("  public:")
-    res.append("    DFA(const int n, const int sigma, const int initial_state,")
-    res.append("        const std::vector<unsigned char> &accepting,")
-    res.append(
-        "        const std::vector<std::vector<int>> &trans, const std::string &key)"
-    )
-    res.append("        : n(n), sigma(sigma), initial_state(initial_state),")
-    res.append("          accepting(accepting), trans(trans), key(key) {")
-    res.append("        assert(n >= 0);")
-    res.append("        assert(sigma >= 0);")
-    res.append("")
-    res.append("        assert(static_cast<int>(accepting.size()) >= n);")
-    res.append("        assert(static_cast<int>(trans.size()) >= n);")
-    res.append("        for (int i = 0; i < n; i += 1) {")
-    res.append("            assert(static_cast<int>(trans[i].size()) >= sigma);")
-    res.append("        }")
-    res.append("    }")
-    res.append("")
-    res.append("    template <std::size_t N, std::size_t SIGMA>")
-    res.append(
-        "    DFA(const int initial_state, const std::array<unsigned char, N> &acc,"
-    )
-    res.append(
-        "        const std::array<std::array<int, SIGMA>, N> &tr, const std::string &key)"
-    )
-    res.append("        : n(N), sigma(SIGMA), initial_state(initial_state),")
-    res.append(
-        "          accepting(acc.begin(), acc.end()), trans(N, std::vector<int>(SIGMA)),"
-    )
-    res.append("          key(key) {")
-    res.append("        // acc と tr の要素数は決まっている")
-    res.append("        for (int i = 0; i < N; i += 1) {")
-    res.append("            for (int j = 0; j < SIGMA; j += 1) {")
-    res.append("                trans[i][j] = tr[i][j];")
-    res.append("            }")
-    res.append("        }")
-    res.append("    }")
-    res.append("")
-    res.append("    int state_size() const noexcept { return n; }")
-    res.append("    int alphabet_size() const noexcept { return sigma; }")
-    res.append(
-        "    int index_of_initial_state() const noexcept { return initial_state; }"
-    )
-    res.append("")
-    res.append("    const std::string &get_key() const & noexcept { return key; }")
-    res.append("")
-    res.append("    bool is_accepting(int src) const {")
-    res.append("        if (src < 0 || src >= n) {")
-    res.append("            return false;")
-    res.append("        }")
-    res.append("        return static_cast<bool>(accepting[src]);")
-    res.append("    }")
-    res.append("")
-    res.append("    int next(int src, int label) const {")
-    res.append("        if (src < 0 || src >= n) {")
-    res.append("            return -1;")
-    res.append("        }")
-    res.append("        if (label < 0 || label >= sigma) {")
-    res.append("            return -1;")
-    res.append("        }")
-    res.append("        return trans[src][label];")
-    res.append("    }")
-    res.append("};")
-    res.append("")
-    res.append("class DFAs {")
-    res.append("  private:")
-    res.append("    std::vector<DFA> dfas;")
-    res.append("")
-    res.append("  public:")
-    res.append(
-        "    const std::vector<DFA> &operator()() const & noexcept { return dfas; }"
-    )
-    res.append("")
-    res.append("    int index_of(const std::string &key) const {")
-    res.append("        auto it = std::find_if(")
-    res.append("            dfas.begin(), dfas.end(),")
-    res.append(
-        "            [&key](const DFA &dfa) -> bool { return dfa.get_key() == key; });"
-    )
-    res.append("        if (it != dfas.end()) {")
-    res.append("            return static_cast<int>(it - dfas.begin());")
-    res.append("        } else {")
-    res.append("            return -1;")
-    res.append("        }")
-    res.append("    }")
-    res.append("")
-    res.append("    const DFA &get(const std::string &key) const & {")
-    res.append("        int i = index_of(key);")
-    res.append("        if (i < 0) {")
-    res.append("            std::abort();")
-    res.append("        }")
-    res.append("        return dfas[i];")
-    res.append("    }")
-    res.append("")
-    res.append("    template <std::size_t N, std::size_t SIGMA>")
-    res.append("    void register_dfa(const int initial_state,")
-    res.append("                      const std::array<unsigned char, N> &acc,")
-    res.append("                      const std::array<std::array<int, SIGMA>, N> &tr,")
-    res.append("                      const std::string &key) {")
-    res.append("        DFA dfa(initial_state, acc, tr, key);")
-    res.append("")
-    res.append("        int i = index_of(key);")
-    res.append("        if (i >= 0) {")
-    res.append("            std::abort(); // already exists")
-    res.append("        }")
-    res.append("        dfas.emplace_back(dfa);")
-    res.append("    }")
-    res.append("};")
-    res.append("")
-    res.append("inline DFAs &dfas() {")
-    res.append("    static DFAs dfas;")
-    res.append("    return dfas;")
-    res.append("}")
+    res.append("""\
+class DFA {
+  private:
+    int n;
+    int sigma;
+    int initial_state;
+
+    std::vector<unsigned char> accepting;
+    std::vector<std::vector<int>> trans;
+
+    std::string key;
+
+    // initial_state や trans などが範囲外となることも一応許容する
+
+  public:
+    DFA(const int n, const int sigma, const int initial_state,
+        const std::vector<unsigned char> &accepting,
+        const std::vector<std::vector<int>> &trans, const std::string &key)
+        : n(n), sigma(sigma), initial_state(initial_state),
+          accepting(accepting), trans(trans), key(key) {
+        assert(n >= 0);
+        assert(sigma >= 0);
+
+        assert(static_cast<int>(accepting.size()) >= n);
+        assert(static_cast<int>(trans.size()) >= n);
+        for (int i = 0; i < n; i += 1) {
+            assert(static_cast<int>(trans[i].size()) >= sigma);
+        }
+    }
+
+    template <std::size_t N, std::size_t SIGMA>
+    DFA(const int initial_state, const std::array<unsigned char, N> &acc,
+        const std::array<std::array<int, SIGMA>, N> &tr, const std::string &key)
+        : n(N), sigma(SIGMA), initial_state(initial_state),
+          accepting(acc.begin(), acc.end()), trans(N, std::vector<int>(SIGMA)),
+          key(key) {
+        // acc と tr の要素数は決まっている
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < SIGMA; j += 1) {
+                trans[i][j] = tr[i][j];
+            }
+        }
+    }
+
+    int state_size() const noexcept { return n; }
+    int alphabet_size() const noexcept { return sigma; }
+    int index_of_initial_state() const noexcept { return initial_state; }
+
+    const std::string &get_key() const & noexcept { return key; }
+
+    bool is_accepting(int src) const {
+        if (src < 0 || src >= n) {
+            return false;
+        }
+        return static_cast<bool>(accepting[src]);
+    }
+
+    int next(int src, int label) const {
+        if (src < 0 || src >= n) {
+            return -1;
+        }
+        if (label < 0 || label >= sigma) {
+            return -1;
+        }
+        return trans[src][label];
+    }
+};
+
+class DFAs {
+  private:
+    std::vector<DFA> dfas;
+
+  public:
+    const std::vector<DFA> &operator()() const & noexcept { return dfas; }
+
+    int index_of(const std::string &key) const {
+        auto it = std::find_if(
+            dfas.begin(), dfas.end(),
+            [&key](const DFA &dfa) -> bool { return dfa.get_key() == key; });
+        if (it != dfas.end()) {
+            return static_cast<int>(it - dfas.begin());
+        } else {
+            return -1;
+        }
+    }
+
+    const DFA &get(const std::string &key) const & {
+        int i = index_of(key);
+        if (i < 0) {
+            std::abort();
+        }
+        return dfas[i];
+    }
+
+    template <std::size_t N, std::size_t SIGMA>
+    void register_dfa(const int initial_state,
+                      const std::array<unsigned char, N> &acc,
+                      const std::array<std::array<int, SIGMA>, N> &tr,
+                      const std::string &key) {
+        DFA dfa(initial_state, acc, tr, key);
+
+        int i = index_of(key);
+        if (i >= 0) {
+            std::abort(); // already exists
+        }
+        dfas.emplace_back(dfa);
+    }
+};
+
+inline DFAs &dfas() {
+    static DFAs dfas;
+    return dfas;
+}
+""")
     res.append(f"}} // namespace {namespace}")
     res.append("")
 
