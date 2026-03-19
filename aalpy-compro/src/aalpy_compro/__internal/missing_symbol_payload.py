3ed34bb264cedfda7f4fd59135d745a4665326f9
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-03-19 10:24:26 +0900

    `MissingSymbolPayload` に `__instance` を追加

--- aalpy-compro/src/aalpy_compro/__internal/missing_symbol_payload.py
+++ aalpy-compro/src/aalpy_compro/__internal/missing_symbol_payload.py
@@ -1,4 +1,4 @@
-from typing import Final
+from typing import ClassVar, Final, Self
 
 
 class MissingSymbolPayload:
@@ -7,6 +7,24 @@ class MissingSymbolPayload:
     """
 
     __slots__ = ()
+    __instance: ClassVar[Self | None] = None
+
+    def __new__(cls) -> Self:
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __copy__(self) -> Self:
+        return self
+
+    def __deepcopy__(self, _: dict[int, object]) -> Self:
+        return self
+
+    def __reduce__(self) -> tuple[type[Self], tuple[()]]:
+        return (type(self), ())
+
+    def __repr__(self) -> str:
+        return "MISSING_SYMBOL_PAYLOAD"
 
 
 MISSING_SYMBOL_PAYLOAD: Final = MissingSymbolPayload()
