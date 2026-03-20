715af492210bba40fbe3cc59d3da94fca4ae4152
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-03-20 20:55:09 +0900

    変数名を `dot` から `dot_star` に変更

--- examples/tenka1_2019_c/property.py
+++ examples/tenka1_2019_c/property.py
@@ -3,6 +3,6 @@ from aalpy_compro import Regex
 alphabet = [0, 1]  # 0: 白, 1: 黒
 
 black_white = Regex.symbol(1) + Regex.symbol(0)
-dot: Regex[int] = Regex.dot().star()
+dot_star: Regex[int] = Regex.dot().star()
 
-regex = ~(dot + black_white + dot)
+regex = ~(dot_star + black_white + dot_star)
