5bbae094a48b234e3ad304b9be4254464344cdfa
Not_Leonian <75620009+NotLeonian@users.noreply.github.com>
2026-07-30 00:09:41 +0900

    Python checks を追加 (#103)
    * pyproject.toml に、チェックから ac-library 配下を除く処理を追加
    
    * README.md に Python checks を追加
    
    * Pyright のエラーを修正

---
+++ aalpy-compro/typings/aalpy/base/__init__.pyi
@@ -0,0 +1,7 @@
+"""Types for the unannotated AALpy base API used by aalpy-compro."""
+
+from .Automaton import Automaton as Automaton
+from .Automaton import AutomatonState as AutomatonState
+from .Automaton import DeterministicAutomaton as DeterministicAutomaton
+from .Oracle import Oracle as Oracle
+from .SUL import SUL as SUL
